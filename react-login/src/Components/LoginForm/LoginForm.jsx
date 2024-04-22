842ca0ef7d486deebd2b9eb4007db960c241d8ed
P3RD3 <sasho32112@gmail.com>
2024-04-22 17:21:51 +0300

    react login form 90%

--- react-login/src/Components/LoginForm/LoginForm.jsx
+++ react-login/src/Components/LoginForm/LoginForm.jsx
@@ -1,7 +1,35 @@
 import react from 'react';
+import './LoginForm.css';
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa6";
+
+
+
 const LoginForm = () => {
     return(
-        <div>LoginForm</div>
+        <div className='wrapper'>
+            <form action="">
+                <h1>Login</h1>
+                <div className='input-box'>
+                    <input type="text" placeholder='Username' required>
+                    </input>
+                    <FaUser className='icon' />
+                </div>
+                <div className='input-box'>
+                    <input type="password" placeholder='Password' required>
+                    </input>
+                    <FaLock className='icon' />
+                </div>
+                <div className='remember-forgot'>
+                    <label><input type='checkbox' /> Remember me </label>
+                    <a href='#'> Forgot password? </a>
+                </div>
+                <button type='submit'>Login</button>
+                <div className='register-link'>
+                    <p>Don't have an account? <a href='#'>Register</a> </p>
+                </div>
+            </form>
+        </div>
     );
 };
 
