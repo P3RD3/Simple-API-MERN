f7b31fd08a0f83d8951f703c127f13e929194ebf
P3RD3 <sasho32112@gmail.com>
2024-05-14 17:34:35 +0300

    added login handle

--- react-login/src/Components/LoginForm/LoginForm.jsx
+++ react-login/src/Components/LoginForm/LoginForm.jsx
@@ -1,35 +1,86 @@
 import './LoginForm.css';
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa6";
+import React, { useRef, useState, useEffect} from 'react';
 
 
+function LoginForm(){
+    const userRef = useRef();
+    const errRef = useRef();
 
-const LoginForm = () => {
-    return(
-        <div className='wrapper'>
-            <form onSubmit=''>
-                <h1>Login</h1>
-                <div className='input-box'>
-                    <input type="text" placeholder='Username' required>
-                    </input>
-                    <FaUser className='icon' />
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(()=>{
+        userRef.current.focus();
+    },[])
+    useEffect(()=>{
+        setErrMsg('');
+    },[user, pwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(user,pwd);
+        setUser('');
+        setPwd('');
+        setSuccess(true);
+    }
+
+return(
+        <>
+        {success ?(
+            <section>
+                <h1>You are logged in!</h1>
+                <br />
+                <p>
+                    <a href='/'> Go to Home</a>
+                </p>
+            </section>
+        ):(
+        <section>
+                <div className='wrapper'>
+                    <form onSubmit={handleSubmit}>
+                        <h1>Login</h1>
+                        <div className='input-box'>
+                            <input 
+                            type="text" 
+                            placeholder='Username'
+                            id='username'
+                            required 
+                            ref={userRef} 
+                            autoComplete='off' 
+                            autoCapitalize='off'
+                            onChange={(e) => setUser(e.target.value)}
+                            value={user}
+                            ></input>
+                            <FaUser className='icon' />
+                        </div>
+                        <div className='input-box'>
+                            <input 
+                            type="password"
+                            id='password'
+                            placeholder='Password' 
+                            required
+                            onChange={(e) => setPwd(e.target.value)}
+                            value={pwd}
+                            ></input>
+                            <FaLock className='icon' />
+                        </div>
+                        <div className='remember-forgot'>
+                            <label><input type='checkbox' /> Remember me </label>
+                            <a href='/forgot'> Forgot password? </a>
+                        </div>
+                        <button type='submit'>Login</button>
+                        <div className='register-link'>
+                            <p>Don't have an account? <a href='/register'>Register</a> </p>
+                        </div>
+                    </form>
                 </div>
-                <div className='input-box'>
-                    <input type="password" placeholder='Password' required>
-                    </input>
-                    <FaLock className='icon' />
-                </div>
-                <div className='remember-forgot'>
-                    <label><input type='checkbox' /> Remember me </label>
-                    <a href='/forgot'> Forgot password? </a>
-                </div>
-                <button type='submit'>Login</button>
-                <div className='register-link'>
-                    <p>Don't have an account? <a href='/register'>Register</a> </p>
-                </div>
-            </form>
-        </div>
-    );
-};
+            </section>
+        )}
+    </>
+)};
 
 export default LoginForm;
